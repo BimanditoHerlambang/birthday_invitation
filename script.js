7964d8e8b1dbe0de8a7fe0cdc664c140e7b724f7
BimanditoHerlambang <bimanditoherlambang@gmail.com>
2024-12-07 12:07:42 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById("rsvpForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+   
+    // Collect RSVP data
+    const name = document.getElementById("name").value;
+    const people = document.getElementById("people").value;
+   
+    // Display confirmation message
+    const confirmationMessage = document.getElementById("confirmationMessage");
+    confirmationMessage.innerHTML = `
+      <p>Thank you <strong>${name}</strong>, for your RSVP!</p>
+      <p>You have confirmed ${people} attendee(s). We look forward to seeing you there!</p>
+      <p>Lets Party With Us!<p>
+    `;
+    confirmationMessage.classList.remove("hidden");
+  });
